cc19e80120a082ffd28c18094d251281b5883e71
unknown <hnguyen166@myseneca.ca>
2024-12-12 01:47:00 -0500

    V3 converting.c

--- converting.c
+++ converting.c
@@ -32,5 +32,20 @@ void converting(void) {
 		}
 	} while (strcmp(doubleString, "q") != 0);
 	printf("*** End of Converting Strings to double Demo ***\n\n");
+
+// V3
+	printf("*** Start of Converting Strings to long Demo ***\n");
+	char longString[BUFFER_SIZE];
+	long longNumber;
+	do {
+		printf("Type the long numeric string (q - to quit):\n");
+		fgets(longString, BUFFER_SIZE, stdin);
+		longString[strlen(longString) - 1] = '\0';
+		if (strcmp(longString, "q") != 0) {
+			longNumber = atol(longString);
+			printf("Converted number is %ld\n", longNumber);
+		}
+	} while (strcmp(longString, "q") != 0);
+	printf("*** End of Converting Strings to long Demo***\n\n");
 } 
 
